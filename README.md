c447089d0613c372a72a2f178c1a929773cd522c
urmasa-tar <nik.goltsev.97@bk.ru>
2024-03-05 12:50:42 +0700

    Start_of_proj

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Introduction_to_CH
+
+
+<h3>I would like to know base code principce of that language<h3>
+
+<h4>Resources</h4>
+
+
---
+++ test_proj/Program.cs
@@ -0,0 +1,39 @@
+﻿// A skeleton of a C# program
+using System;
+
+namespace YourNamespace // What is name space?
+{
+    class YourClass // 
+    {
+    }
+
+    struct YourStruct// 
+    {
+    }
+
+    interface IYourInterface
+    {
+    }
+
+    delegate int YourDelegate();
+
+    enum YourEnum
+    {
+    }
+
+    namespace YourNestedNamespace
+    {
+        struct YourStruct
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Your program starts here...
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
