51c17694ec572d0ad6514438276d5655bdae27f6
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-16 12:55:31 +0300

    Grid_solution

---
+++ Avalonia_solution/Grid_project/Grid_project/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Grid_project.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Avalonia_solution/Grid_project/Grid_project/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Grid_project.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
