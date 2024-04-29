6a8a2b38902a5c3ae3276969f2d31a6e7e97962b
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-29 20:23:38 +0300

    concatination

--- cs_traine_stepik/trening_from_Stepik/Program.cs
+++ cs_traine_stepik/trening_from_Stepik/Program.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿using message;
+using System;
+using static message.str_message;
+using static message.str_int_concat;
 
 class Program
 {
     public static void Main(string[] args)
     {
+        /*
+        str_message mess_concat = new str_message();
+
+        mess_concat.concat();
+        */
+
+        str_int_concat with_int = new str_int_concat();
+
+        with_int.concat();
 
     }
 
---
+++ cs_traine_stepik/trening_from_Stepik/str_message.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace message
+{
+    public class str_message
+    {
+        public void concat()
+        {
+            string[] inputMessages = ReadInput();
+            string message1, message2;
+            message1 = inputMessages[0];
+            message2 = inputMessages[1];
+            string result = message1 + " " + message2;
+
+            Console.WriteLine(result);
+        }
+
+        public string[] ReadInput()
+        {
+            string inputLine = Console.ReadLine();
+
+            return inputLine.Split(" | ");
+        }
+    }
+
+    public class str_int_concat()
+    {
+        public void concat()
+        {
+            string[] inputValues = ReadInput();
+            string message = inputValues[0];
+            int score = int.Parse(inputValues[1]);
+            score = score * 2;
+            string result = message + " " + score.ToString();
+
+            Console.WriteLine(result);
+        }
+
+        public string[] ReadInput()
+        {
+            string inputLine = Console.ReadLine();
+            return inputLine.Split(" | ");
+        }
+    }
+    public class concat_multiple()
+    {
+        public void multiple_read()
+        {
+            string message;
+            int score, bonus;
+
+            string[] inputValues = ReadInput();
+            message = inputValues[0];
+            score = int.Parse(inputValues[1]);
+            bonus = int.Parse(inputValues[2]);
+
+            string result = "";
+
+            Console.WriteLine(result);
+        }
+
+        public string[] ReadInput()
+        {
+            string inputLine = Console.ReadLine();
+            return inputLine.Split(" | ");
+        }
+    }
+}
