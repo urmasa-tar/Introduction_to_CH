f08a7992e32b1716ee0bb5db24ab6cc6d1807ed7
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-30 13:20:42 +0300

    Morning_push

--- cs_traine_stepik/trening_from_Stepik/ruler.cs
+++ cs_traine_stepik/trening_from_Stepik/ruler.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Tar;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace trening_from_Stepik
 {
-    internal class ruler
+    class ruler
     {
+        public void to_star()
+        {
+            string star = "";
+
+            string inp_str = ReadInput();
+
+            int ln = inp_str.Length;
+
+            for (int i = 0; i < ln; i++)
+            {
+                star += "*";
+            }
+
+            Console.WriteLine(star.ToString());
+        }
+
+        public string ReadInput()
+        {
+            string res = "";
+
+            if (Console.ReadLine() is string input)
+            {
+                res = input;
+            }
+
+            return res;
+        }
     }
 }
