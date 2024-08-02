a6605f14072dc2bda90e142877e44b3b01af468d
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-08-02 15:17:05 +0300

    WithExampleOfstatement

---
+++ byThenesCh/regularState/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    /*
+     Example for regular statements
+     Here we gonna use classes Regex and MathcCollections
+
+        Regex regex = new Regex(@"туп(\w*)");
+        MatchCollection matches = regex.Matches(s);
+        if (matches.Count > 0)
+        {
+            foreach (Match match in matches)
+                Console.WriteLine(match.Value);
+        }
+        else
+        {
+            Console.WriteLine("Совпадений не найдено");
+        }
+     */
+}
